6bb32bef63d89fe67269abdf4fe1c59416ac8401
quo0001 <alex.jarrah1@gmail.com>
2023-12-14 00:39:19 -0500

    Added formatter functions to improve compatibility

--- internal/utils.go
+++ internal/utils.go
@@ -1,6 +1,10 @@
 package internal
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func GetCardType(CardNumber any) string {
 	number := fmt.Sprint(CardNumber)
@@ -25,4 +29,79 @@ func GetCardType(CardNumber any) string {
 	}
 
 	return ""
+}
+
+func FormatCardYear(CardYear any, full bool) uint16 {
+	year := fmt.Sprint(CardYear)
+
+	switch full {
+	case true:
+		if len(year) == 2 {
+			year = "20" + year
+		}
+
+	case false:
+		if len(year) == 4 {
+			year = year[2:]
+		}
+	}
+
+	res, _ := strconv.Atoi(year)
+	return uint16(res)
+}
+
+func FormatCardMonth(CardMonth any, full bool) string {
+	month := fmt.Sprint(CardMonth)
+
+	switch full {
+	case true:
+		if len(month) == 1 {
+			month = "0" + month
+		}
+
+	case false:
+		month = strings.TrimLeft(month, "0")
+	}
+
+	return month
+}
+
+func FormatCountry(country string, full bool) string {
+	country = strings.ToUpper(country)
+
+	switch full {
+	case true:
+		if long, exists := CountryNames[country]; exists {
+			country = long
+		}
+
+	case false:
+		for short, long := range CountryNames {
+			if strings.ToUpper(long) == country {
+				country = short
+			}
+		}
+	}
+
+	return country
+}
+
+func FormatState(state string, full bool) string {
+	state = strings.ToUpper(state)
+
+	switch full {
+	case true:
+		if long, exists := StateNames[state]; exists {
+			state = long
+		}
+
+	case false:
+		for short, long := range StateNames {
+			if strings.ToUpper(long) == state {
+				state = short
+			}
+		}
+	}
+
+	return state
 }
